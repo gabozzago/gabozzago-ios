12e9576706cc7c45813fbc99ce41e52de1a43412
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-07-11 17:11:22 +0900

    Initial Commit

---
+++ gabozago-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gabozago-iOS
+//
+//  Created by sunghun on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
