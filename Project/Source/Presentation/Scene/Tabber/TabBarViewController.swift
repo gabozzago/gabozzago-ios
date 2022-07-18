957a56f204ad47e8ae8301012c687c8161a07dba
Mindohyeon <min50875@gmail.com>
2022-07-19 08:40:06 +0900

    탭바 아이템에 해당하는 클래스 생성

---
+++ Project/Source/Presentation/Scene/Tabber/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarViewController.swift
+//  gabozago-iOS
+//
+//  Created by 민도현 on 2022/07/19.
+//  Copyright © 2022 com.wwg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Then
+
+class TabBarViewController: UITabBarController {
+    private let mainVC = MainViewController().then {
+    }
+}
