921401e2baed7272cef6f13ca7d94f501c4e6019
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-07-18 11:22:52 +0900

    tuist

---
+++ Project.swift
@@ -0,0 +1,20 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let projectName = "gabozago-iOS"
+let orginazationIden = "com.wwg"
+
+let project = Project.excutable(
+    name: projectName,
+    platform: .iOS,
+    packages: [
+        .Then,
+        .SnapKit
+    ],
+    product: .app,
+    deploymentTarget: .iOS(targetVersion: "13.5", devices: [.iphone, .ipad]),
+    dependencies: [
+        .SPM.Then,
+        .SPM.SnapKit
+    ]
+)
---
+++ Tuist/ProjectDescriptionHelpers/TargetDependency.swift
@@ -0,0 +1,21 @@
+import ProjectDescription
+
+extension TargetDependency{
+    public struct SPM {}
+}
+
+public extension TargetDependency.SPM{
+    static let SnapKit = TargetDependency.package(product: "SnapKit")
+    static let Then = TargetDependency.package(product: "Then")
+}
+
+public extension Package {
+    static let SnapKit = Package.remote(
+        url: "https://github.com/SnapKit/SnapKit.git",
+        requirement: .upToNextMajor(from: "5.0.1")
+    )
+    static let Then = Package.remote(
+        url: "https://github.com/devxoul/Then.git",
+        requirement: .upToNextMajor(from: "2.7.0")
+    )
+}
