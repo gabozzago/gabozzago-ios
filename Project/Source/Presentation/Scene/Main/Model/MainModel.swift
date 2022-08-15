f021e7218ee3e906cd02215d9b5b4ea051cd2753
Mindohyeon <min50875@gmail.com>
2022-08-15 23:09:39 +0900

    :sparkles: conflict 해결
    mainUI 부분이 날라가서 깃허브에서 클론함

--- Project/Source/Presentation/Scene/Main/Model/MainModel.swift
+++ Project/Source/Presentation/Scene/Main/Model/MainModel.swift
@@ -1,4 +1,9 @@
 import UIKit
 
 struct MainModel {
+    var id: String
+    var title: String
+    var image: String
+    var address: String
+    var description: String
 }
--- Project/Source/Presentation/Scene/Main/VM/MainViewModel.swift
+++ Project/Source/Presentation/Scene/Main/VM/MainViewModel.swift
@@ -1,4 +1,14 @@
 import UIKit
 
-class MainViewModel: baseViewModel {
+final class MainViewModel: baseViewModel {
+    
+    var heartViewIsVisible = Observable(false)
+    
+    func heartViewIsVisibleDidTap() {
+        heartViewIsVisible.value.toggle()
+    }
+    
+    func pushCreatePostVC() {
+        coordinator.navigate(to: .createPostIsRequired)
+    }
 }
