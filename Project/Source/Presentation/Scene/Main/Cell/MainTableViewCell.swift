e4b6d8627cbd87e389ef7da0274c5cd4c7237253
Mindohyeon <min50875@gmail.com>
2022-08-23 17:27:22 +0900

    :sparkles: scale 변경

--- Project/Source/Presentation/Scene/Main/Cell/MainTableViewCell.swift
+++ Project/Source/Presentation/Scene/Main/Cell/MainTableViewCell.swift
@@ -55,7 +55,7 @@ final class MainTableViewCell: baseTableViewCell<PostModel> {
     // MARK: - Method
     func animateView(_ viewToAnimate:UIView) {
         UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn, animations: {
-            viewToAnimate.transform = CGAffineTransform(scaleX: 1.08, y: 1.08)
+            viewToAnimate.transform = CGAffineTransform(scaleX: 1.32, y: 1.32)
         }) { (_) in
             UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.3,
                            initialSpringVelocity: 10, options: .curveEaseOut, animations: {
