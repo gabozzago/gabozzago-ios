c20d2ded9fc453f49f035874f30516e8f35bf8ca
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-08-30 23:10:15 +0900

    :package: Add package Moya
    Add package Moya

--- Tuist/ProjectDescriptionHelpers/TargetDependency.swift
+++ Tuist/ProjectDescriptionHelpers/TargetDependency.swift
@@ -8,6 +8,7 @@ public extension TargetDependency.SPM{
     static let SnapKit = TargetDependency.package(product: "SnapKit")
     static let Then = TargetDependency.package(product: "Then")
     static let GoogleSignIn = TargetDependency.package(product: "GoogleSignIn")
+    static let Moya = TargetDependency.package(product: "Moya")
 }
 
 public extension Package {
@@ -22,5 +23,9 @@ public extension Package {
     static let GoogleSignIn = Package.remote(
         url: "https://github.com/google/GoogleSignIn-iOS",
         requirement: .upToNextMajor(from: "6.2.2")
+    )
+    static let Moya = Package.remote(
+        url: "https://github.com/Moya/Moya",
+        requirement: .upToNextMajor(from: "15.0.1")
     )
 }
